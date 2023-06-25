9f23bad047e24bdfa72c4c5930a9a322ec78329e
Level80Starmie <70716546+Level80Starmie@users.noreply.github.com>
2023-06-25 13:13:57 +0100

    Update GuaranteedHint.cs
    Adding argument to class to pull the loaded Settings values, then pulling resources from the ActiveGame subdirectory

--- GSTHD/GuaranteedHint.cs
+++ GSTHD/GuaranteedHint.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
@@ -7,25 +7,27 @@ namespace GSTHD
 {
     class GuaranteedHint : PictureBox
     {
+		private readonly Settings Settings;
         List<string> ListImageName = new List<string>();
         bool isMouseDown = false;
 
-        Size GuaranteddHintSize;
+        Size GuaranteedHintSize;
 
-        public GuaranteedHint(ObjectPoint data)
+        public GuaranteedHint(ObjectPoint data, Settings settings)
         {
+			Settings = settings;
             if(data.ImageCollection != null)
                 ListImageName = data.ImageCollection.ToList();
 
-            GuaranteddHintSize = data.Size;
+            GuaranteedHintSize = data.Size;
 
             this.BackColor = Color.Transparent;
             if (ListImageName.Count > 0)
             {
                 this.Name = ListImageName[0];
-                this.Image = Image.FromFile(@"Resources/" + ListImageName[0]);
+                this.Image = Image.FromFile(@Settings.ActiveGame+"/Resources/" + ListImageName[0]);
                 this.SizeMode = PictureBoxSizeMode.StretchImage;
-                this.Size = GuaranteddHintSize;
+                this.Size = GuaranteedHintSize;
             }            
             this.Location = new Point(data.X, data.Y);
             this.TabStop = false;
