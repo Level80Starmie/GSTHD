0dfb7102b917e309e865ea2ab533e230c51d2bc9
Level80Starmie <70716546+Level80Starmie@users.noreply.github.com>
2023-06-25 13:13:38 +0100

    Update GossipStone.cs
    Changing where the application pulls resources from, to the ActiveGame subdirectory

--- GSTHD/GossipStone.cs
+++ GSTHD/GossipStone.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
@@ -96,11 +96,11 @@ namespace GSTHD
         {
             if (HoldsImage)
             {
-                Image = Image.FromFile(@"Resources/" + HeldImageName);
+                Image = Image.FromFile(@Settings.ActiveGame+"/Resources/" + HeldImageName);
             }
             else
             {
-                Image = Image.FromFile(@"Resources/" + ImageNames[ImageIndex]);
+                Image = Image.FromFile(@Settings.ActiveGame+"/Resources/" + ImageNames[ImageIndex]);
             }
         }
 
